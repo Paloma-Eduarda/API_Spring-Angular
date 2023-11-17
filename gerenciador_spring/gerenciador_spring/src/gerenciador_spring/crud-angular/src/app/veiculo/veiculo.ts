b3a1d848671356673c9e3ec699629e8d4eda0682
Paloma Eduarda da Silva Tavares <125306650+Paloma-Eduarda@users.noreply.github.com>
2023-11-17 00:24:47 -0300

    Delete gerenciador_spring/gerenciador_spring/src/gerenciador_spring directory

--- gerenciador_spring/gerenciador_spring/src/gerenciador_spring/crud-angular/src/app/veiculo/veiculo.ts
+++
@@ -1,9 +0,0 @@
-export class Veiculo{
-  id:number | null = null;
-  modelo: string | null = null;
-  marca: string | null = null;
-  cor: string | null = null;
-  placa: string | null = null;
-  reserva: string | null = null;
-  anoDeFabricacao: number | null = null;
-}
--- gerenciador_spring/gerenciador_spring/src/gerenciador_spring/gerenciador_spring/src/main/java/paloma/com/gerenciador_spring/repository/VeiculoRepository.java
+++
@@ -1,9 +0,0 @@
-package paloma.com.gerenciador_spring.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import paloma.com.gerenciador_spring.entities.VeiculoEntity;
-
-public interface VeiculoRepository  extends JpaRepository<VeiculoEntity, Integer>{
-    
-    
-}
